b73a60e7e2da7340c655e1e87d12a333d3cda6dd
Vengeful-Pancake <thinh2000tiger@gmail.com>
2022-07-13 21:55:54 +0700

    Add functional openable screen

--- open.py
+++ open.py
@@ -1,5 +1,8 @@
 import pygame
-
+import win32api
+import win32con
+import win32gui
+import time
 from pygame.locals import *
 from tkinter import *
 
@@ -7,4 +10,40 @@ root = Tk()
 scr_w = root.winfo_screenwidth()
 scr_h = root.winfo_screenheight()
 
+transparency = (255, 0, 128)  # Transparency color
+
+
 pygame.init()
+
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN) # For borderless, use pygame.NOFRAME
+
+hwnd = pygame.display.get_wm_info()["window"]
+win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*transparency), 0, win32con.LWA_COLORKEY)
+
+fullscreen = True
+run = 1
+while run:
+    check = 0
+    screen.fill(transparency)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = 0
+        if event.type == KEYDOWN:
+            if event.key == K_F11:
+                if fullscreen and not check:
+                    scr_w = 1600
+                    scr_h = 900
+                    screen = pygame.display.set_mode((scr_w, scr_h))
+                    fullscreen = False
+                    check = 1
+                    time.sleep(0.1)
+                if not fullscreen and not check:
+                    scr_w = root.winfo_screenwidth()
+                    scr_h = root.winfo_screenheight()
+                    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+                    fullscreen = True
+                    time.sleep(0.1)
+                    check = 1
+
+    pygame.display.update()

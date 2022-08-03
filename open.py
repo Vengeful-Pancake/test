b434cf85664a7ff946a0db94707b4a6714a7e6cf
Vengeful-Pancake <thinh2000tiger@gmail.com>
2022-08-03 21:48:06 +0700

    00

--- open.py
+++ open.py
@@ -8,51 +8,65 @@ import speech_recognition
 import numpy as np
 import cv2
 
+from image import Image
 from pygame.locals import *
 from tkinter import *
  
 
-
+#find screen height and width
 root = Tk()
 scr_w = root.winfo_screenwidth()
 scr_h = root.winfo_screenheight()
 
+#start library pygame 
 pygame.init()
 
-screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN) # For borderless, use pygame.NOFRAME
+# For borderless, use pygame.NOFRAME
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN) 
 clock = pygame.time.Clock()
-transparency = (255, 0, 128)  # Transparency color
+# Transparency color
+transparency = (255, 0, 128)  
 pygame.display.set_caption("Z-Wings")
 font = pygame.font.Font("font\coders_crux.ttf", 40)
 fullscreen = True
 run = 1
-
+#neccessary for transparant screen, add layer
 hwnd = pygame.display.get_wm_info()["window"]
 win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*transparency), 0, win32con.LWA_COLORKEY)
 
-def img(x,y,z):
-    global scr_w, scr_h
-    # read image
-    img = pygame.image.load(z)
-    # height, width of image
-    height = img.get_height()
-    width = img.get_width()
-    o = 1
-    h=height
-    while h > y:
-        o = o - 0.01
-        w = width * o
-        h = height * o
-        img_resize = pygame.transform.scale(img, (w, h))
-    textRect = img_resize.get_rect()
-    textRect.center = (scr_w-w/2-w/16, scr_h-h/2-h/7.5)
-    screen.blit(img_resize,textRect)
-    return
-    
+#function to put sprite
+def img(x,y,z,scr_h,scr_w,i):
+    if not i:
+        # read image
+        img = pygame.image.load(z)
+        # height, width of image
+        height = img.get_height()
+        width = img.get_width()
+        o = 1
+        h=height
+        while h > y:
+            o = o - 0.01
+            w = width * o
+            h = height * o
+            img_resize = pygame.transform.scale(img, (w, h))
+        # while h < y:
+        #     o = o - 0.01
+        #     w = width * o
+        #     h = height * o
+        #     img_resize = pygame.transform.scale(img, (w, h))
+        textRect = img_resize.get_rect()
 
-def text(texter):
-    global scr_h, scr_w, font, screen
+        #w/16, h/7.5 is the width between eadge
+        textRect.center = (scr_w-w/2-w/16, scr_h-h/2-h/7.5)
+        screen.blit(img_resize,textRect)
+        return
+    
+    return 1
+    
+#function to put text
+def text(texter, scr_h, scr_w, font):
+    global screen
     text = font.render(texter, True, (0,0,0), (100,10,255))
     textRect = text.get_rect()
     textRect.center = (scr_w // 2, scr_h // 2)
@@ -85,8 +99,10 @@ while run:
                     time.sleep(0.1)
                     check = 1
     
-    text("rep")
-    img(tolx, toly, 'img/sprite.png')
+    text("test text, this text should not disappear at anytime!!!", scr_h, scr_w, font)
+    
     pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(scr_w-tolx, scr_h-toly, tolx, toly),1)
+    img(tolx, toly, 'img/sprite.png', scr_h, scr_w, 0)
+    
     pygame.display.update()
     clock.tick(60)
